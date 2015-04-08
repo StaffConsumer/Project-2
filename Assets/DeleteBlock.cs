cb99c722ca4e3e2218178be59157f7049b772686
William Corrin <soccerpkmn@gmail.com>
2015-04-07 20:14:28 -0700

    Large Push
    Im PUSHIN boys yall move that dope ok?

---
+++ Assets/DeleteBlock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteBlock : MonoBehaviour {
+
+	public Camera Default;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		GameObject obj = col.gameObject;
+		if(obj.tag == "Cube")
+		{
+			Destroy (obj);
+		}
+		else if(obj.tag == "Player")
+		{
+			Default.enabled = true;
+			Destroy(obj);
+		}
+	}
+}
