3acda0777dea0193512db95631abc8d26deef3a9
mash189 <marsh189@mail.chapman.edu>
2015-05-16 22:29:47 -0700

    Fixed Timers

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -6,14 +6,21 @@ public class Timer : MonoBehaviour {
 
 	Text t;
 	int i;
+	public int StartTimer;
 	// Use this for initialization
 	void Start () {
 		t = GetComponent<Text> ();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		t.text = "TIME: " + (i / 60).ToString ();
+	 void Update () {
+		Debug.Log (i / 60);
+		if (i / 60 >= StartTimer) 
+		{
+
+			t.text = "TIME: " + (i / 60 - StartTimer).ToString ();
+		}
 	}
 
 	void FixedUpdate()
